0b4e2e231ea3845855ae88bb1b38aa8540a58e36
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2024-02-23 16:20:07 -0500

    Just added some css and created the footer. Added some styling and Ids for the footer.

--- sohr-app/src/index.js
+++ sohr-app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
 import Message from './components/newthing.tsx';
 import ListGroup from './components/ListGroup.tsx';
+import Footer from './components/Footer.tsx'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ListGroup />
-  </React.StrictMode>
+    <Message />
+    <Footer />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
